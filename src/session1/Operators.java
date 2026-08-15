e17619b4d71e19d60d800871ae74ea3b6c65664b
كارل 31157430 <UG_31157430@f-eng.tanta.edu.eg>
2026-08-15 17:56:32 +0300

    Completed session1 assignments

---
+++ src/session1/Operators.java
@@ -0,0 +1,14 @@
+package session1;
+
+public class Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        int a = 20, b = 10;
+
+        System.out.println("The addition of a and b is " + (a + b));        //30
+        System.out.println("The substract of a and b is " + (a - b));       //10
+        System.out.println("The multiplication of a and b is " + (a * b));  //200
+        System.out.println("The division of a and b is " + (a / b));        //2
+        System.out.println("The remainder of a divided by b is " + (a % b)); //0
+    }
+}
