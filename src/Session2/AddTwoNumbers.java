909eae301a66fd8c1d8113c419884e706d6789d0
كارل 31157430 <UG_31157430@f-eng.tanta.edu.eg>
2026-08-15 22:07:41 +0300

    Add Session2 assignment

---
+++ src/Session2/AddTwoNumbers.java
@@ -0,0 +1,30 @@
+package Session2;
+
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        boolean response;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            // What we need
+            System.out.println("Please Enter First Number");
+            int num1 = scanner.nextInt();
+
+            System.out.println("Please Enter Second Number");
+            int num2 = scanner.nextInt();
+
+            // calculation
+            int sum = num1 + num2;
+            System.out.println("The sum is " + sum);
+
+            System.out.println("Do you want to continue ? 1-true 2-false");
+            int choice = scanner.nextInt();
+            response = (choice == 1);
+
+        } while (response);
+
+        scanner.close();
+    }
+}
---
+++ src/Session2/ConditionalStatements.java
@@ -0,0 +1,117 @@
+package Session2;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    public static void main(String[] args) {
+//        EX2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Enter Number To Check ");
+//        int numberToCheck = scanner.nextInt();
+//
+//        if (numberToCheck % 2 == 0) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("Odd");
+//        }
+//
+//        scanner.close();
+
+
+
+        //        EX3
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Enter Number To Check ");
+//        int numberToCheck = scanner.nextInt();
+//        if (numberToCheck > 0) {
+//            System.out.println("Positive");
+//        } else if (numberToCheck < 0) {
+//            System.out.println("Negative");
+//        } else {
+//            System.out.println("Zero");
+//        }
+//
+//        scanner.close();
+
+
+        // Ex4
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Please Enter first number ");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.println("Please Enter second number ");
+//        int num2 = scanner.nextInt();
+//
+//        System.out.println("Please Enter third number ");
+//        int num3 = scanner.nextInt();
+//
+//        if (num1 > num2 && num1 > num3) {
+//            System.out.println("The first number is the largest");
+//        } else if (num2 > num1 && num2 > num3) {
+//            System.out.println("The second number is the largest");
+//        } else {
+//            System.out.println("The third number is the largest");
+//        }
+//
+//        scanner.close();
+
+        // Ex5
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Enter Day Number ");
+//        int dayNumber = scanner.nextInt();
+//
+//        if (dayNumber == 1) {
+//            System.out.println("Saturday");
+//        } else if (dayNumber == 2) {
+//            System.out.println("Sunday");
+//        } else if (dayNumber == 3) {
+//            System.out.println("Monday");
+//        } else if (dayNumber == 4) {
+//            System.out.println("Tuesday");
+//        } else if (dayNumber == 5) {
+//            System.out.println("Wednesday");
+//        } else if (dayNumber == 6) {
+//            System.out.println("Thursday");
+//        } else if (dayNumber == 7) {
+//            System.out.println("Friday");
+//        } else {
+//            System.out.println("Invalid Day Number");
+//        }
+//
+//        scanner.close();
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please Enter Day Number ");
+//        int dayNumber = scanner.nextInt();
+//
+//        switch (dayNumber) {
+//            case 1:
+//                System.out.println("Saturday");
+//                break;
+//            case 2:
+//                System.out.println("Sunday");
+//                break;
+//            case 3:
+//                System.out.println("Monday");
+//                break;
+//            case 4:
+//                System.out.println("Tuesday");
+//                break;
+//            case 5:
+//                System.out.println("Wednesday");
+//                break;
+//            case 6:
+//                System.out.println("Thursday");
+//                break;
+//            case 7:
+//                System.out.println("Friday");
+//                break;
+//            default:
+//                System.out.println("Invalid Day Number");
+//        }
+//
+//        scanner.close();
+    }
+}
